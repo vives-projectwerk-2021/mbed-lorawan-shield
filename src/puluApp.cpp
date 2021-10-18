a1869be0bfb2def7bdd3cf1e2ad96c3837d37098
Dimi Catrysse <dimicatrysse@outlook.com>
2021-10-18 19:48:49 +0200

    Implement initialization of LoRaWAN with keys from EEPROM

--- src/puluApp.cpp
+++ src/puluApp.cpp
@@ -5,10 +5,10 @@
 
 namespace Pulu {
     App::App(Pulu::config config) :
-        node(config.lorawan.keys, config.lorawan.pins),
-        sensors(),
-        eeprom(config.eeprom.sda, config.eeprom.scl, config.eeprom.address)
-    {
+        eeprom(config.eeprom.sda, config.eeprom.scl, config.eeprom.address),
+        node(get_LoRaWAN_keys(), config.lorawan.pins),
+        sensors()
+    {   
         
     };
     
@@ -19,5 +19,41 @@ namespace Pulu {
             node.send(message.getMessage(), message.getLength());
             ThisThread::sleep_for(30s);
         }
+    }
+
+    SimpleLoRaWAN::LoRaWANKeys App::get_LoRaWAN_keys() {
+        MbedCRC<POLY_32BIT_ANSI, 32> mbedCRC;
+
+        char data[45];
+        if(eeprom.read(data, 45)) {
+            return {};
+        }
+
+        SimpleLoRaWAN::LoRaWANKeys keys;
+        struct LoRaWANCRC {
+            uint32_t devEui;
+            uint32_t appEui;
+            uint32_t appKey;
+        };
+        LoRaWANCRC crc;
+        LoRaWANCRC crcCheck;
+        memcpy(keys.devEui, data+1, 8);
+        memcpy(&crc.devEui, data+9, 4);
+        memcpy(keys.appEui, data+13, 8);
+        memcpy(&crc.appEui, data+21, 4);
+        memcpy(keys.appKey, data+25, 16);
+        memcpy(&crc.appKey, data+41, 4);
+
+        mbedCRC.compute((void *)keys.devEui, 8, &crcCheck.devEui);
+        mbedCRC.compute((void *)keys.appEui, 8, &crcCheck.appEui);
+        mbedCRC.compute((void *)keys.appKey, 16, &crcCheck.appKey);
+
+        if(crc.devEui == crcCheck.devEui
+        && crc.appEui == crcCheck.appEui
+        && crc.appKey == crcCheck.appKey)
+        {
+            return keys;
+        }
+        return {};
     }
 };
--- src/puluApp.h
+++ src/puluApp.h
@@ -10,10 +10,14 @@ namespace Pulu {
             App(Pulu::config config);
 
             void run();
+
+        private:
+            SimpleLoRaWAN::LoRaWANKeys get_LoRaWAN_keys();
         
         private:
+            EEPROM_24AA64 eeprom;
             SimpleLoRaWAN::Node node;
             Pulu::sensorManager sensors;
-            EEPROM_24AA64 eeprom;
+
     };
 };
