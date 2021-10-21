a81f4386226404d50e1330d9245559e673b9d571
Dimi Catrysse <dimicatrysse@outlook.com>
2021-10-21 15:23:06 +0200

    Removed unused LoRaWAN keys from config

--- src/config.example.h
+++ src/config.example.h
@@ -4,13 +4,7 @@
 
 namespace Pulu {
     struct config {
-        struct lorawan_config {
-            SimpleLoRaWAN::LoRaWANKeys keys = { 
-                { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },         // devEui
-                { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },         // appEui
-                { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } // appKey
-            };
-
+        struct lorawan {
             SimpleLoRaWAN::Pinmapping pins = { D11, D12, D13, A0, A1, D2, D3 };  // mosi, miso, clk, nss, reset, dio0, dio1
         } lorawan;
         struct eeprom {
