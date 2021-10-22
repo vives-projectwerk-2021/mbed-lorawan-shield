185e8eaaf98f1e59b35f5d53d91fdf18a70db595
Dimi Catrysse <dimicatrysse@outlook.com>
2021-10-22 09:49:18 +0200

    Update default config

--- src/config.example.h
+++ src/config.example.h
@@ -8,9 +8,9 @@ namespace Pulu {
             SimpleLoRaWAN::Pinmapping pins = { D11, D12, D13, A0, A1, D2, D3 };  // mosi, miso, clk, nss, reset, dio0, dio1
         } lorawan;
         struct eeprom {
-            PinName sda = NC;
-            PinName scl = NC;
-            uint16_t address = 0;
+            PinName sda = D14;
+            PinName scl = D15;
+            uint16_t address = 0b1010000 << 1;
         } eeprom;
     };
 };
