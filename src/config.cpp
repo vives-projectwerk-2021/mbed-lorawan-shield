5a8fe97d88ac6ead23a90050f7b50cdd4a461e76
Dimi Catrysse <dimicatrysse@outlook.com>
2021-11-23 14:38:53 +0100

    Set configuration for prototype board

--- src/config.cpp
+++ src/config.cpp
@@ -11,8 +11,8 @@ namespace Pulu {
             .pins = { D11, D12, D13, A0, A1, D2, D3 }  // mosi, miso, clk, nss, reset, dio0, dio1
         },
         .eeprom = {
-            .i2c = &Pulu::i2c_1,
-            .address = 0b1010000 << 1
+            .i2c = &Pulu::i2c_3,
+            .address = 0b1010001 << 1
         },
         .sensors = {
             .temperature = {{
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include "mbed.h"
+#include "USBSerial.h"
+USBSerial serial;
+
+FileHandle *mbed::mbed_override_console(int) {
+  return &serial;
+}
+
 #include "puluAppLauncher.h"
 
 Pulu::AppLauncher appLauncher;
