71e8595df1a687222eabcb4972accedfa6a6b34c
Dimi Catrysse <dimicatrysse@outlook.com>
2021-12-17 18:43:28 +0100

    Save battery levels in eeprom and send status codes based on computed slope

--- src/puluApp.cpp
+++ src/puluApp.cpp
@@ -4,6 +4,7 @@
 #include "LoRaMessage.h"
 #include "converters.h"
 #include "identifier.h"
+#include "LineairRegression.h"
 
 namespace Pulu {
     App::App(Pulu::EEPROM_Config eeprom_config) :
@@ -23,6 +24,37 @@ namespace Pulu {
             app_DEBUG("requesting sensor measurement");
             auto measurement = sensors.do_measurement();
             app_DEBUG("sensor measurement done");
+            app_DEBUG("checking battery slope");
+            uint8_t bat_val = ((100*measurement.values.battery)/3.6);
+            if(eeprom.write_battery_level(bat_val)) {
+                measurement.status.battery = Sensors::sensorStatus::BATTERY_SAVE_READ_ERROR;
+                app_DEBUG("failed to save battery level");
+            }
+            else {
+                bool err;
+                auto battery_levels_array = eeprom.read_battery_levels(&err);
+                if(err) {
+                    measurement.status.battery = Sensors::sensorStatus::BATTERY_SAVE_READ_ERROR;
+                    app_DEBUG("failed to read saved battery levels");
+                }
+                else {
+                    uint8_t* battery_levels = (uint8_t*)calloc(battery_levels_array.size(), sizeof(uint8_t));
+                    for (uint8_t i = 0; i<battery_levels_array.size(); i++) {
+                        battery_levels[i] = battery_levels_array[i];
+                    }
+                    float slope = LineairRegression::get_slope(battery_levels, (int)battery_levels_array.size(), &err);
+                    if(err) {
+                        measurement.status.battery = Sensors::sensorStatus::BATTERY_SLOPE_ERROR;
+                        app_DEBUG("failed to compute slope of battery levels");
+                    }
+                    free(battery_levels);
+                    if(slope<-0.02 && measurement.values.battery < 0.98) {
+                        measurement.status.battery = Sensors::sensorStatus::BATTERY_LOW_ALERT;
+                        app_DEBUG("LOW BATTERY LEVEL");
+                    }
+                }
+            }
+            app_DEBUG("checked battery slope");
             app_DEBUG("converting measurement to LoRaMessage");
             LoRaMessage message = Pulu::Converters::measurement_to_LoRaMessage(measurement);
             app_DEBUG("converted measurement to LoRaMessage");
